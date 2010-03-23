93a86671f9684c14646a19ad62414bd30f626e05
Mark Cipolla <mark@markcipolla.com>
2010-03-23 18:14:39 +1100

    Bug fixes, and it also plays the blame game and shows who broke the build

--- hudson_global_dashboard.rb
+++ hudson_global_dashboard.rb
@@ -12,15 +12,18 @@ set :views => File.dirname(__FILE__) + '/views/'
 get '/' do
   # All the internal IP addresses of your Hudson machines.
   @individual_hudsons = [
-    "192.168.53.214:8081", 
-    "192.168.51.6:8080", 
     "192.168.53.187:8081",
+    "192.168.53.214:8081",
+    "192.168.51.6:8080",
     "192.168.52.30:8888"
   ]
   
   @builds = []
   @individual_hudsons.each do |host|
     host_results = JSON.parse(Net::HTTP.get(URI.parse("http://#{host}/api/json?depth=1")))
+    host_results["jobs"].each do |job|
+      job.merge!("hudson_host" => "#{host}")
+    end
     @builds += host_results["jobs"]
   end
   erb :index
