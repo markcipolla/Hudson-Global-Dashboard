62e61c48665fed0853227b318bdd18cc0c0eefab
Mark Cipolla <mark@markcipolla.com>
2010-04-15 15:01:27 +1000

    Moved the dirty code that was requesting and parsing the "changes" page for who broke a build back to the model, where it belongs

--- app/controllers/dashboard.rb
+++ app/controllers/dashboard.rb
@@ -24,15 +24,28 @@ HudsonDashboard.controllers :dashboard do
     Buildserver.all.each do |server|
       begin
         server_data = JSON.parse(Net::HTTP.get(URI.parse("#{URI.escape(server.address)}/api/json?depth=1")))
+      rescue Exception => e
+        server_data = nil
+      end
+
+      if server_data
         server_data["jobs"].each do |job|
+
           job.merge!("hudson_host" => "#{server.address}")
+
+          if job['lastSuccessfulBuild'] && job['color'] == 'red'
+            res = Net::HTTP.get(URI.parse("#{job['hudson_host']}/job/#{URI.escape(job['name'])}/#{job['lastSuccessfulBuild']["number"] + 1}/changes"))
+            build_broken_by_username = res.to_s.match(/by \<a href="\/user\/([^\/]*)\//) || ""
+            if build_broken_by_username && build_broken_by_username != ""
+              job.merge!("build_broken_by" => "#{build_broken_by_username}" )
+            end
+          end
+
         end
         @builds_details << server_data["jobs"]
-      rescue Exception => e
-        nil
       end
     end
-    
+
     render 'dashboard/index'
   end
 
