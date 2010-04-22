a303699ef62ea6d5a6e0efbdfb1f42414a93206d
Mark Cipolla <mark@markcipolla.com>
2010-04-22 14:37:00 +1000

    Moved scripts, and added backgroundpulsing for building builds...

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,21 @@
 // Put your application scripts here
+(function heightSorter() {
+  windowH = $(window).height();
+  docH = $(document).height();
+  if ( docH == windowH ) {
+
+    numberOfBuilds = $('article').size();
+    newSize = (windowH / numberOfBuilds) - 20;
+    $('article').height(newSize);
+    $('ul').addClass("resizedList");
+  }
+})()
+
+function colourPulser() {
+  $('.blue_anime').animate({opacity: 0.4}, 400 ).animate({opacity: 1}, 370 );
+  $('.aborted_anime').animate({opacity: 0.4}, 400 ).animate({opacity: 1}, 370 );
+  $('.red_anime').animate({opacity: 0.4}, 400 ).animate({opacity: 1}, 370 );
+}
+
+setTimeout('colourPulser()', 1000);
+setTimeout('location.reload()', 30000);
